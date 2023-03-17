02c0fd825593fe898538da644b0c77e151f2c34b
Bradley Barker <Bradley.Barker@thoughtware.co.za>
2023-03-17 09:18:45 +0200

    try again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
 import { useState } from 'react';
-import prompts from './Prompt';
+
+function prompts(prompts = "pr1.txt", num = 10, artists = 2) {
+  return new Promise((resolve, reject) => {
+    let prompt = [];
+    if (prompts === "pr1.txt") {
+      fetch("pr1.txt")
+        .then((response) => response.text())
+        .then((text) => {
+          prompt = text.split("\n");
+          const generatedPrompts = generate(prompt, num, artists);
+          resolve(generatedPrompts);
+        })
+        .catch(reject);
+    } else if (prompts === "pr2.txt") {
+      fetch("pr2.txt")
+        .then((response) => response.text())
+        .then((text) => {
+          prompt = text.split("\n");
+          const generatedPrompts = generate(prompt, num, artists);
+          resolve(generatedPrompts);
+        })
+        .catch(reject);
+    }
+
+    function generate(prompt, num_word, artists) {
+      let vocab = prompt.length;
+      let generated = [];
+      let artists_num = 0;
+
+      while (
+        [...new Set(generated)].length < num_word &&
+        generated.length < vocab
+      ) {
+        let rand = Math.floor(Math.random() * vocab);
+        if (
+          prompt[rand].startsWith("art by") &&
+          artists_num < artists &&
+          !generated.includes(prompt[rand])
+        ) {
+          artists_num += 1;
+          generated.push(prompt[rand]);
+        } else if (
+          !prompt[rand].startsWith("art by") &&
+          !generated.includes(prompt[rand])
+        ) {
+          generated.push(prompt[rand]);
+        }
+      }
+
+      return [...new Set(generated)];
+    }
+  });
+}
 
 function App() {
   const [generatedPrompts, setGeneratedPrompts] = useState([]);
 
   const handleGeneratePrompts = () => {
-    const promptsArray = prompts("pr1.txt", 10, 2);
-    setGeneratedPrompts(promptsArray);
+    prompts("pr1.txt", 10, 2)
+      .then((promptsArray) => {
+        setGeneratedPrompts(promptsArray);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
@@ -20,5 +77,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
--- src/Prompt.js
+++ src/Prompt.js
@@ -1,47 +1,53 @@
 function prompts(prompts = "pr1.txt", num = 10, artists = 2) {
-    let prompt = [];
-    if (prompts === "pr1.txt") {
-      return fetch("pr1.txt")
-        .then((response) => response.text())
-        .then((text) => {
-          prompt = text.split("\n");
-          return generate(prompt, num, artists);
-        });
-    } else if (prompts === "pr2.txt") {
-      return fetch("pr2.txt")
-        .then((response) => response.text())
-        .then((text) => {
-          prompt = text.split("\n");
-          return generate(prompt, num, artists);
-        });
-    }
-  
-    function generate(prompt, num_word, artists) {
-      let vocab = prompt.length;
-      let generated = [];
-      let artists_num = 0;
-  
-      while (
-        [...new Set(generated)].length < num_word &&
-        generated.length < vocab
-      ) {
-        let rand = Math.floor(Math.random() * vocab);
-        if (
-          prompt[rand].startsWith("art by") &&
-          artists_num < artists &&
-          !generated.includes(prompt[rand])
-        ) {
-          artists_num += 1;
-          generated.push(prompt[rand]);
-        } else if (
-          !prompt[rand].startsWith("art by") &&
-          !generated.includes(prompt[rand])
-        ) {
-          generated.push(prompt[rand]);
-        }
+    return new Promise((resolve, reject) => {
+      let prompt = [];
+      if (prompts === "pr1.txt") {
+        fetch("pr1.txt")
+          .then((response) => response.text())
+          .then((text) => {
+            prompt = text.split("\n");
+            const generatedPrompts = generate(prompt, num, artists);
+            resolve(generatedPrompts);
+          })
+          .catch(reject);
+      } else if (prompts === "pr2.txt") {
+        fetch("pr2.txt")
+          .then((response) => response.text())
+          .then((text) => {
+            prompt = text.split("\n");
+            const generatedPrompts = generate(prompt, num, artists);
+            resolve(generatedPrompts);
+          })
+          .catch(reject);
       }
   
-      return [...new Set(generated)].join(", ");
-    }
+      function generate(prompt, num_word, artists) {
+        let vocab = prompt.length;
+        let generated = [];
+        let artists_num = 0;
+  
+        while (
+          [...new Set(generated)].length < num_word &&
+          generated.length < vocab
+        ) {
+          let rand = Math.floor(Math.random() * vocab);
+          if (
+            prompt[rand].startsWith("art by") &&
+            artists_num < artists &&
+            !generated.includes(prompt[rand])
+          ) {
+            artists_num += 1;
+            generated.push(prompt[rand]);
+          } else if (
+            !prompt[rand].startsWith("art by") &&
+            !generated.includes(prompt[rand])
+          ) {
+            generated.push(prompt[rand]);
+          }
+        }
+  
+        return [...new Set(generated)];
+      }
+    });
   }
-export default prompts;  
+  
